2c3ca87434eb8ff1273cafdedd0baa5c148c4583
lkhayaweb92 <50678097+lkhayaweb92@users.noreply.github.com>
2023-07-17 23:08:36 +0200

    Update settings.py
    Rimossi codici sensibili

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ ALLOW_ADMIN_RESTORE = True
 TEST                =    0
 
 if TEST:
-    BOT_TOKEN       = '6194032742:AAHK4yr1w4lQzVrilmvadaAv_hf1L7XO84M' #lavoristefano
+    BOT_TOKEN       = 'Test_token'
 else:
-    BOT_TOKEN = '6240165109:AAHoQNa6TmhKm1pevWwsU6GvcnqOgSDezrA' #botscuola
+    BOT_TOKEN = 'Bot_Reale'
 
 CHANNEL_LOG = '' #canalelog dati
-DB_NAME = 'scuolabot' #nomeperildatabase
-PROJECT_NAME = 'CentroScolasticoAspi' #nomeprogetto
+DB_NAME = '' #nomeperildatabase
+PROJECT_NAME = '' #nomeprogetto
